724da9a1d14fdf79a90d31f38d2b55224897475a
iwannabetheguy161 <linhmonmon2201@gmail.com>
2020-12-08 15:20:53 +0700

    Solution of DCTDN2

---
+++ DCTDN2.cpp
@@ -0,0 +1,25 @@
+// this solutions was submitted and accepted on DCTDN2 problem
+#include <bits/stdc++.h>
+
+int main() {
+    using namespace std;
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+#ifdef _iwannabetheguy
+    freopen("in", "r", stdin);
+#endif
+
+    int n; cin >> n;
+    int a[n];
+    for(int i=0; i<n; ++i) cin >> a[i];
+    int ans=1;
+    vector<int> dp(n+1, INT_MAX);
+    dp[0]=INT_MIN;
+    for(int i=0; i<n; ++i) {
+        int k=lower_bound(dp.begin(), dp.end(), a[i])-dp.begin();
+        dp[k]=a[i];
+        ans=max(ans, k);
+    }
+    cout << ans << '\n';
+
+    return 0;
+}
